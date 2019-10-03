880af69ffc4ccdc70ad51169ccced7b6dace9e31
Kelvin-The-Engineer <kdoverjr@ufl.edu>
2019-10-02 23:51:05 -0400

    Add files via upload

---
+++ server/config/config.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  db: {
+      uri: 'mongodb+srv://student:12131995@cen3031-eoceo.mongodb.net/test?retryWrites=true&w=majority', //place the URI of your mongo database here.
+//place the URI of your mongo database here.
+  }, 
+  openCage: {
+    key: 'e2ccd4e8c8ce445b91be4995c54332fd' //place your openCage public key here - Sign-up for a free key https://opencagedata.com/
+  },
+  port: 8080
+};
